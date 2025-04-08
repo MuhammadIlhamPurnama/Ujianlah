9a8c9392ff87d65c6f0ada120f05250895dd71fd
Muhammad Ilham Purnama <ilham.purnama@digits.id>
2025-04-09 06:55:03 +0700

    fix (backend) : fix cors error

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,13 @@ const port = process.env.PORT || 5000;
 const MDB_KEY = process.env.MDB_KEY;
 
 const corsOptions = {
-  origin: 'https://ujianlah.vercel.app', // your frontend URL
+  origin: 'https://ujianlah.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://ujianlah.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, auth-token');
-  next();
-});
-
 
 // Connect to MongoDB
 mongoose.connect(MDB_KEY)
